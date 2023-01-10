fe2c40908733dcc1984e7fd71fc84f2e1dac6cea
nix <mail@tmake.ru>
2023-01-10 14:51:51 +0200

    move build to target path

--- .build/.mk/develop.mk
+++ .build/.mk/develop.mk
@@ -3,5 +3,5 @@
 sh: ## start shell in backend
 	docker-compose exec mysqldump-slice sh
 
-build: ## build
-	docker run --rm -v ${PWD}:/mysqldump-slice -w /mysqldump-slice lunny/centos-go:latest go build -o build/slice cmd/main.go
+build: ## build slice for centos
+	docker run --rm -v ${PWD}:/mysqldump-slice -w /mysqldump-slice lunny/centos-go:latest go build -o target/slice cmd/main.go
